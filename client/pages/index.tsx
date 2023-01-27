5e6be955d99d96830c3851b477bac18877b80cdb
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-26 17:55:18 -0800

    add register page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -15,32 +15,83 @@ export default function Home() {
                     <h1>feedbook</h1>
                     <h3>Connect with friends and the world around you</h3>
                 </div>
-                <div className={login.input_panel}>
-                    <form>
-                        <input
-                            name="email"
-                            type="email"
-                            placeholder="Email Address"
-                            minLength={2}
-                            required
-                        />
-                        <input
-                            name="password"
-                            type="password"
-                            placeholder="Password"
-                            minLength={6}
-                            required
-                        />
-                        <button className={login.login_button}>Login In</button>
-                    </form>
-                    <hr />
-                    <button className={login.new_account_button}>
-                        Create new Account
-                    </button>
-                    <button className={login.guest_button}>
-                        Sign in as Guest
-                    </button>
-                </div>
+                {signUp ? (
+                    <div className={login.login_panel}>
+                        <h2>Signup</h2>
+                        <p>It is fast and easy</p>
+                        <form>
+                            <input
+                                name="name"
+                                type="text"
+                                placeholder="Full Name"
+                                minLength={1}
+                                required
+                            />
+                            <input
+                                name="email"
+                                type="email"
+                                placeholder="Email Address"
+                                minLength={2}
+                                required
+                            />
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                minLength={6}
+                                required
+                            />
+                            <button
+                                type="submit"
+                                className={login.new_account_button}
+                            >
+                                Sign Up
+                            </button>
+                        </form>
+                        <hr />
+                        <button
+                            className={login.guest_button}
+                            onClick={() => setSignUp(false)}
+                        >
+                            Back to Login
+                        </button>
+                    </div>
+                ) : (
+                    <div className={login.login_panel}>
+                        <form>
+                            <input
+                                name="email"
+                                type="email"
+                                placeholder="Email Address"
+                                minLength={2}
+                                required
+                            />
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                minLength={6}
+                                required
+                            />
+                            <button
+                                type="submit"
+                                className={login.login_button}
+                            >
+                                Login
+                            </button>
+                        </form>
+                        <hr />
+                        <button
+                            className={login.new_account_button}
+                            onClick={() => setSignUp(true)}
+                        >
+                            Create new Account
+                        </button>
+                        <button className={login.guest_button}>
+                            Sign in as Guest
+                        </button>
+                    </div>
+                )}
             </div>
         </>
     );
