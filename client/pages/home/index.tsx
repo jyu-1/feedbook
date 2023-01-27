fcb8a0c502cae69e9110ee8f0fc3699b62266d4d
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-26 17:43:03 -0800

    add signin page

---
+++ client/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Home Page</title>
+            </Head>
+            <div>Home page</div>
+        </>
+    );
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,47 @@
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
+import login from "@/styles/login.module.scss";
+import { useState } from "react";
 
 export default function Home() {
+    const [signUp, setSignUp] = useState(false);
+
     return (
         <>
-            <div>Feedbook</div>
+            <Head>
+                <title>Feedbook | Login or Signup</title>
+            </Head>
+            <div className={login.main}>
+                <div>
+                    <h1>feedbook</h1>
+                    <h3>Connect with friends and the world around you</h3>
+                </div>
+                <div className={login.input_panel}>
+                    <form>
+                        <input
+                            name="email"
+                            type="email"
+                            placeholder="Email Address"
+                            minLength={2}
+                            required
+                        />
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            minLength={6}
+                            required
+                        />
+                        <button className={login.login_button}>Login In</button>
+                    </form>
+                    <hr />
+                    <button className={login.new_account_button}>
+                        Create new Account
+                    </button>
+                    <button className={login.guest_button}>
+                        Sign in as Guest
+                    </button>
+                </div>
+            </div>
         </>
     );
 }
