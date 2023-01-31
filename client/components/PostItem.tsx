1de3e1ff52a7f32d006ef81b5a4a61c3a9598281
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-30 22:51:49 -0800

    add update post and format dates

--- client/components/PostItem.tsx
+++ client/components/PostItem.tsx
@@ -1,4 +1,6 @@
 import style from "@/styles/postcontainer.module.scss";
+import { formatDistanceToNow } from "date-fns";
+import { useRef, useState } from "react";
 import { useAuthContext } from "./AuthContext";
 
 interface ItemType {
@@ -25,6 +27,55 @@ interface PostItemProps {
 
 export default function PostItem({ post, setPost }: PostItemProps) {
     const { user } = useAuthContext();
+    const updateRef = useRef<HTMLInputElement>(null);
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleUpdate = async () => {
+        try {
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_SERVER_IP}/api/post/${post._id}`,
+                {
+                    method: "PATCH",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        name: user ? user.email : "null",
+                        message: updateRef.current
+                            ? updateRef.current.value
+                            : post.message,
+                    }),
+                }
+            );
+
+            const json = await response.json();
+
+            if (!response.ok) {
+                console.log(json.error);
+                setShowEdit(false);
+            }
+
+            if (response.ok) {
+                setShowEdit(false);
+                setPost((prev) =>
+                    prev.map((item) => {
+                        if (item._id === post._id) {
+                            return {
+                                ...item,
+                                message: updateRef.current
+                                    ? updateRef.current.value
+                                    : post.message,
+                            };
+                        }
+                        return item;
+                    })
+                );
+            }
+        } catch (err: unknown) {
+            if (err instanceof Error) console.log(err.message);
+            console.log("Server is starting. Please wait about 20 seconds.");
+            setShowEdit(false);
+        }
+    };
+
     const handleDelete = async () => {
         try {
             const response = await fetch(
@@ -53,10 +104,30 @@ export default function PostItem({ post, setPost }: PostItemProps) {
                 <div>{post.uploadImage}</div>
                 <div>
                     <div className={style.name}>{post.name}</div>
-                    <div className={style.date}>{post.createdAt}</div>
+                    <div className={style.date}>
+                        {formatDistanceToNow(new Date(post.createdAt), {
+                            addSuffix: true,
+                        })}
+                    </div>
                 </div>
             </div>
-            <div className={style.message}>{post.message}</div>
+            {showEdit ? (
+                <div>
+                    <input
+                        defaultValue={post.message}
+                        type="text"
+                        name="message"
+                        placeholder="Edit message"
+                        minLength={1}
+                        maxLength={500}
+                        required
+                        ref={updateRef}
+                    />
+                    <button onClick={handleUpdate}>Update</button>
+                </div>
+            ) : (
+                <div className={style.message}>{post.message}</div>
+            )}
             {post.uploadImage && (
                 <>
                     <div className={style.photo}>Photo</div>
@@ -72,6 +143,15 @@ export default function PostItem({ post, setPost }: PostItemProps) {
             <div className={style.like_comment_buttons}>
                 <button>Like</button>
                 <button>Comment</button>
+                {showEdit ? (
+                    <button onClick={() => setShowEdit(false)}>
+                        Cancel Update
+                    </button>
+                ) : (
+                    <button onClick={() => setShowEdit(true)}>
+                        Update Post
+                    </button>
+                )}
                 <button onClick={handleDelete}>Delete Post</button>
             </div>
             <hr />
@@ -81,7 +161,12 @@ export default function PostItem({ post, setPost }: PostItemProps) {
                         <div>
                             <span className={style.name}>{comment.name}</span>{" "}
                             <span className={style.date}>
-                                {comment.createdAt}
+                                {formatDistanceToNow(
+                                    new Date(comment.createdAt),
+                                    {
+                                        addSuffix: true,
+                                    }
+                                )}
                             </span>
                         </div>
                         <div>{comment.message}</div>
