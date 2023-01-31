da6961b9e4272b87a09ad5a0c44b22c7afa30f99
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-30 19:23:10 -0800

    fetch users from server and add left sidenav

--- client/components/FriendList.tsx
+++ client/components/FriendList.tsx
@@ -1,130 +1,37 @@
+import { useState, useEffect } from "react";
 import style from "@/styles/friendlist.module.scss";
 
+interface UserType {
+    _id: string;
+    name: string;
+}
+
 export default function FriendList() {
+    const [users, setUsers] = useState<UserType[]>([]);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_SERVER_IP}/api/user/list`
+            );
+            const json = await response.json();
+
+            if (response.ok) {
+                setUsers(json);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
     return (
         <div className={style.friend_list}>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
+            {users &&
+                users.map((user) => (
+                    <div className={style.friend_item} key={user._id}>
+                        {user.name}
+                    </div>
+                ))}
         </div>
     );
 }
--- client/components/SideNav.tsx
+++ client/components/SideNav.tsx
@@ -1,130 +1,39 @@
 import style from "@/styles/sidenav.module.scss";
+import Link from "next/link";
 
 export default function SideNav() {
     return (
         <div className={style.nav}>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
-            <div>SideNav</div>
+            <div className={style.nav_item}>
+                <Link href="/">Home</Link>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://www.youtube.com/">YouTube</a>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://www.google.com/">Google</a>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://www.amazon.com/">Amazon</a>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://www.facebook.com/">Facebook</a>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://www.instagram.com/">Instagram</a>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://nodejs.org/en/docs/">Node Docs</a>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://reactjs.org/">React Docs</a>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://expressjs.com/">Express Docs</a>
+            </div>
+            <div className={style.nav_item}>
+                <a href="https://www.mongodb.com/docs/">MongoDB Docs</a>
+            </div>
         </div>
     );
 }
